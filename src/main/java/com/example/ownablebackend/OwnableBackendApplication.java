7bfb0bc2bf4836c8fa3fa2291e0da7118a14e05c
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-01 07:27:12 +0300

    Initial commit

---
+++ src/main/java/com/example/ownablebackend/OwnableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ownablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnableBackendApplication.class, args);
+	}
+
+}
