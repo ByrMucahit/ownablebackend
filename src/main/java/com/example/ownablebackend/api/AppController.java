d029986398b4315405e51f46311632e2ab1c38e6
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-04 22:35:30 +0300

    Add mail service

---
+++ src/main/java/com/example/ownablebackend/api/AppController.java
@@ -0,0 +1,47 @@
+package com.example.ownablebackend.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @GetMapping("/contact")
+    public String  showContactForm() {
+        return "contact_form";
+    }
+
+    @PostMapping("/contact")
+    public String submitContact(HttpServletRequest request) {
+        String fullName = request.getParameter("fullName");
+        String email = request.getParameter("email");
+        String subject = request.getParameter("subject");
+        String content = request.getParameter("content");
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("m.mucahitbayar@gmail.com");
+        message.setTo("mm1.bayar@gmail.com");
+
+        String mailSubject = fullName + " has sent a message";
+        String mailContent = "Sender Name: " + fullName + "\n";
+        mailContent += "Sender E-mail: "+ email + "\n";
+        mailContent += "Subject: "+ subject + "\n";
+        mailContent += "Content: "+ content + "\n";
+
+        message.setSubject(mailSubject);
+        message.setText(mailContent);
+
+        javaMailSender.send(message);
+        return "message";
+    }
+}
